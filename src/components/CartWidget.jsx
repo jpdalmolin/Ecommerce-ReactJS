61712089c6ce4522aaaedcc76e6c71089298ec23
Juan Pablo Dal Molin <54445366+jpdalmolin@users.noreply.github.com>
2021-07-12 02:56:00 -0300

    Add files via upload

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import carro from './carro.svg'
+import './CartWidget.css'
+
 
 function CartWidget() {
     return (
-        <div>
-            <icon carrito en navvar />
+        <div classname="cartwidget">
+            <img src={carro} alt='cart' classname='cartwidget' class="max-width: 100%"></img>
+            
         </div>
     )
 }
