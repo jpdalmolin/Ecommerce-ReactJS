a4083e249e7bffc79f1e987fc59677762d78062b
Juan Pablo Dal Molin <54445366+jpdalmolin@users.noreply.github.com>
2021-07-24 17:04:12 -0300

    Add files via upload

---
+++ servicios/getMocks.js
@@ -0,0 +1,11 @@
+import { productos } from "../components/productos"
+
+const task=new Promise((res)=>{
+    res(productos)
+}, 2000)
+
+export const getMocks=()=>{
+    return(
+        task
+    )
+}
