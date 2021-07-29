0c9e1512fc86e50a5e97cd43dddfc150b4059aec
Juan Pablo Dal Molin <54445366+jpdalmolin@users.noreply.github.com>
2021-07-28 21:01:59 -0300

    Add files via upload

--- container/ItemDetailContainer.jsx
+++ container/ItemDetailContainer.jsx
@@ -1,22 +1,28 @@
 import React, {useState,useEffect} from 'react'
 import ItemDetail from '../components/ItemDetail'
 import {getMocks} from '../servicios/getMocks'
-import { productos } from '../components/productos'
+import{useParams} from 'react-router-dom'
+
 
 function ItemDetailContainer() {
     const [item,setItem]=useState([])
-  
+    const{detalleId}=useParams()
 useEffect(() => {
-    
-   getMocks()
-   .then(resp=>setItem(resp))
-},[])
+    if(detalleId===undefined){
+        getMocks()
+   .then(res=>setItem(res))
+    }else{
+        getMocks()
+        .then(res=>setItem(res.filter(it=>it.id===detalleId)))
+    }
+   
+},[detalleId])
 
-console.log(item)
+console.log(detalleId)
 return (
     <div>
-        {item.map(elemento => <ItemDetail  nombre={elemento.nombre}  image={elemento.image}
-stock={elemento.stock} />)}
+        {item.map(elemento => <ItemDetail   nombre={elemento.nombre}  image={elemento.image}
+stock={elemento.stock} precio={elemento.precio}/>)}
         </div>
     )
 }
