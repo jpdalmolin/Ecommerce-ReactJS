466ce386e78cb8a0fd297e1dd36212f106c3fd41
Juan Pablo Dal Molin <54445366+jpdalmolin@users.noreply.github.com>
2021-08-04 14:50:20 -0300

    Add files via upload

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { NotiContext } from '../context/CartContext'
+
 
 function Cart() {
-    return (
-        <div>
-            
-        </div>
+    const {cartList, guardarItem, price, size} = useContext(NotiContext)
+    console.log(cartList)
+    console.log(price())
+    console.log(size())
+    return  (
+        <>
+           {cartList.map(item =>(
+               <>
+                <div className="row" key={item.productos.id}> 
+                    <p className="col">{item.productos.nombre}</p>  
+                    <p className="col">{item.quantity}</p>  
+                    <p className="col">{item.productos.precio}</p>  
+                </div>
+                <hr/>
+                </>
+            ))}
+        </>
     )
 }
 
 export default Cart
-
