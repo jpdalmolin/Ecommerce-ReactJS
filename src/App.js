494af7a984dfdab1bc65db25d1ac82f144ec98e7
Juan Pablo Dal Molin <54445366+jpdalmolin@users.noreply.github.com>
2021-08-04 14:49:21 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import './App.css';
 import NavBar from './components/NavBar.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from './components/ItemListcontainer'
-import {useState} from 'react'
+import { useEffect,useState,useContext } from 'react';
 import React from 'react';
 import ItemDetailContainer from './container/ItemDetailContainer';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import {NotiContext} from './context/CartContext';
+import CartContextProvider from './context/CartContext';
+import Cart from './components/Cart';
+import UseNotiContext from './context/CartContext';
+
 
 function Titulo(){
 return(
@@ -16,13 +21,17 @@ return(
 function App() {
   const [contador, setContador]=useState(0)
   const [fecha, setFecha]=useState(null)
+
+
+  
 function handleCount(){
 setContador(contador+1)
 let date=Date();
 setFecha(date)
 }
   return (
-    <Router>
+    <UseNotiContext>
+   <Router>
       <Titulo/> 
       <NavBar/>
       <div className="App">
@@ -30,22 +39,32 @@ setFecha(date)
       {fecha}
       <button onClick={handleCount} >Hora</button>
         <Switch>
+        <Route exact path="/">
+     <ItemListContainer greeting='Bienvenido a mi tienda' user='Rodrigo' />
+     </Route>
     <Route exact path={`/detail/:detalleId`} component={ItemDetailContainer}>
  
       </Route>
-      <Route exact path="/">
-     <ItemListContainer greeting='Bienvenido a mi tienda' user='Rodrigo' />
-     </Route>
-     <Route exact path="/">
-     <ItemListContainer greeting='Bienvenido a mi tienda' user='Rodrigo' />
-     </Route>
      <Route exact path="/category/:categoryId">
      <ItemListContainer greeting='Bienvenido a mi tienda' user='Rodrigo' />
      </Route>
+    
+     <Route exact path={`/cart`} component={Cart}>
+ 
+      </Route>
+      
     </Switch>
+
+    
+   
+     
+    
+ 
    
     </div>
     </Router>
+    </UseNotiContext>
+  
   );
 }
 
